c7b0eadf05eb895b6deb109537497b1a643dd492
Alvaro <alvaro@eliminalia.com>
2019-05-23 14:03:20 +0200

    Proyecto, referencias y Main Core CRUD

---
+++ QueueNotificationService/QueueNotificationService/Core/MQueueNotification.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueNotificationService
+{
+    [BsonIgnoreExtraElements]
+    class MQueueNotification
+    {
+        public string _id { get; set; }
+        public DateTime fecha { get; set; }
+        public bool leido { get; set; }
+        public int prioridad { get; set; }
+        public ObjectId from { get; set; }
+        public ObjectId to { get; set; }
+        public string mensaje { get; set; }
+        public ObjectId serviceID { get; set; }
+        public string tipoServicio { get; set; }
+        public int toType { get; set; } //0 sera un contacto (mensaje para un contacto) 1 es para un trabajador.
+
+    }
+}
---
+++ QueueNotificationService/QueueNotificationService/Program.cs
@@ -0,0 +1,69 @@
+﻿using Grapevine.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueNotificationService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                var v = "v0.1.0.0";
+                var nameService = "Queue Notification Service";
+                Console.Title = nameService + " " + v;
+                Console.WriteLine("     Q U E U E   N O T I F I C A T I O N   S E R V I C E    " + v);
+                var serverStandar = new RestServer();
+
+                var input = "";
+                while ((input = Console.ReadLine()) != "q")
+                {
+                    switch (input)
+                    {
+                        case "start":
+                            Console.WriteLine("Starting service...");
+                            serverStandar.Port = "5009";
+                            serverStandar.Host = "*";
+                            serverStandar.Start();
+                            Console.Title = "[ON]  " + nameService + " " + v;
+
+                            break;
+
+
+                        case "start --log":
+                            Console.WriteLine("Starting service...");
+
+                            using (var server = new RestServer())
+                            {
+                                server.Port = "5009";
+                                server.Host = "*";
+                                server.LogToConsole().Start();
+                                Console.Title = "[ON]  " + nameService + " " + v;
+                                Console.ReadLine();
+                                server.Stop();
+                            }
+
+                            break;
+                        case "stop":
+                            Console.WriteLine("Stopping service...");
+                            serverStandar.Stop();
+                            Console.Title = nameService + " " + v;
+                            break;
+                        case "--version":
+                            Console.WriteLine(v);
+
+                            break;
+                        default:
+                            Console.WriteLine(String.Format("Unknown command: {0}", input));
+                            break;
+                    }
+
+                }
+            }
+        }
+    }
+}
